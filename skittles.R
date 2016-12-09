ba68749ea3bd0b3d98edf20cd7f4f5e8475f203d
tiedyedguy <tiedyedguy@users.noreply.github.com>
2016-12-09 11:20:16 -0500

    Update to the R
    To also fix the Pak issue.

--- skittles.R
+++ skittles.R
@@ -10,7 +10,7 @@ library(ggplot2)
 skipalette<-c("#c0043f","#e64808","#f1be02","#048207","#441349")
 
 # Rename data headers, and grab the overall mean
-names(skittles)<-c("Pak","Strawberry","Orange","Lemon","Apple","Grape")
+names(skittles)<-c("Pack","Strawberry","Orange","Lemon","Apple","Grape")
 netmean<-mean(c(skittles$Strawberry,skittles$Orange,skittles$Lemon,skittles$Apple,skittles$Grape))
 
 # Summarize data into frame "skitsum" for the final plot.
@@ -21,8 +21,8 @@ skitsum$flavor <- factor(skitsum$flavor,
                          c("Strawberry","Orange","Lemon","Apple","Grape"))
 
 # Generate "skitbin". "skitbin" will allow us to plot a neat "Raw Results" diagram in the first plot
-skitmelt<-melt(skittles,id="Pak")
-skitbin<-data.frame("Pak"=NA,"variable"=NA,"value"=NA)
+skitmelt<-melt(skittles,id="Pack")
+skitbin<-data.frame("Pack"=NA,"variable"=NA,"value"=NA)
 for(n in 1:nrow(skitmelt)){
   skitbin<-rbind(skitbin,skitmelt[rep(n,skitmelt$value[n]),])   }
 skitbin<-skitbin[2:nrow(skitbin),]
@@ -31,13 +31,13 @@ skitbin$variable <- factor(skitbin$variable,
 rm(n);rm(skitmelt)
 
 # Convert raw data into a "Raw results" pictogram via "skitbin"
-ggplot(skitbin,aes(x=Pak,y=variable))+
+ggplot(skitbin,aes(x=Pack,y=variable))+
   geom_jitter(aes(fill=variable),height=5/16,width=5/16,shape=21,size=3)+
   geom_tile(color="grey75",alpha=0)+
   scale_fill_manual("Flavor",values=skipalette)+
   guides(fill="none")+
   scale_x_continuous(breaks=1:36,minor_breaks=seq(.5,36.5,1))+
-  scale_y_discrete(limits=rev(levels(melt(skittles,id="Pak")$variable)))+
+  scale_y_discrete(limits=rev(levels(melt(skittles,id="Pack")$variable)))+
   labs(title="Skittles Counts",
        subtitle="Raw Results",
        x="Pack Number",y="Flavor",
@@ -46,13 +46,13 @@ ggplot(skitbin,aes(x=Pak,y=variable))+
   theme(panel.grid=element_blank())
 
 # Convert raw data into heatmap, mildly more pleasing than "skitbin"
-ggplot(melt(skittles,id="Pak"),aes(x=Pak,y=variable))+
+ggplot(melt(skittles,id="Pack"),aes(x=Pack,y=variable))+
   geom_tile(aes(fill=variable,alpha=value),color="white")+
   geom_text(aes(label=value),size=3)+
   scale_fill_manual("Flavor",values=skipalette)+
   guides(alpha="none",fill="none")+
   scale_x_continuous(breaks=1:36,minor_breaks=NULL)+
-  scale_y_discrete(limits=rev(levels(melt(skittles,id="Pak")$variable)))+
+  scale_y_discrete(limits=rev(levels(melt(skittles,id="Pack")$variable)))+
   labs(title="Skittles Counts",
        subtitle="Raw Results Converted into Heatmap",
        x="Pack Number",y="Flavor",
@@ -60,7 +60,7 @@ ggplot(melt(skittles,id="Pak"),aes(x=Pak,y=variable))+
   theme_bw()
 
 # Convert data into stacked bar chart. Yet even more pleasing.
-ggplot(melt(skittles,id="Pak"),aes(Pak,value))+
+ggplot(melt(skittles,id="Pack"),aes(Pack,value))+
   geom_bar(stat="identity",aes(fill=variable),alpha=.8)+
   scale_fill_manual("Flavor",values=skipalette)+
   labs(title="Skittles Counts",
@@ -71,7 +71,7 @@ ggplot(melt(skittles,id="Pak"),aes(Pak,value))+
   theme_bw()
 
 # Plot data in a violinplot, with added dotplot for effect.
-ggplot(melt(skittles,id="Pak"),aes(variable,value))+
+ggplot(melt(skittles,id="Pack"),aes(variable,value))+
   geom_violin(aes(fill=variable),alpha=.8)+
   geom_dotplot(binaxis="y",stackdir="center",aes(fill=variable))+
   scale_fill_manual("Flavor",values=skipalette)+
